a417dd2f395019395a4e290b02586cb50247c890
laille2 <louis.laillet@gmail.com>
2020-10-08 11:31:19 +0200

    Ajout des librairies

--- src/com/inti/entities/Ferme.java
+++ src/com/inti/entities/Ferme.java
@@ -3,10 +3,18 @@
  */
 package com.inti.entities;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+
 /**
  * @author IN-LL-029
  *
  */
+@Entity
 public class Ferme {
+	private Long idFerme;
+	private String nom;
+	private Date dateAchat;
 
 }
