0c115bc5cefbe9936f3ea6b26d16075e484335dc
unknown <pauline.muyl195@gmail.com>
2020-10-08 13:33:49 +0200

    Ajout de @Id et @GeneratedValue

--- src/com/inti/entities/Ferme.java
+++ src/com/inti/entities/Ferme.java
@@ -7,10 +7,14 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
-
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Ferme implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long idFerme;
 	private String nom;
 	private Date dateAchat;
